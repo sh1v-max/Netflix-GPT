f7e1f1ac1c3e723a931d663bca7dee36deea1558
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-13 18:28:21 +0530

    login header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-          <header className="bg-black text-white px-6 py-4 shadow-md">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
-        {/* Logo */}
-        <h1 className="text-3xl font-extrabold text-red-600 tracking-wider cursor-pointer">
-          NETFLIX
-        </h1>
-
-        {/* Right Side - You can add buttons/menus here */}
-        <nav className="flex items-center space-x-4">
-          <button className="bg-red-600 hover:bg-red-700 px-4 py-2 rounded text-sm font-semibold transition duration-300">
-            Sign In
-          </button>
-        </nav>
-      </div>
-    </header>
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black'>
+      <img
+        className="w-44"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
     </div>
   )
 }
