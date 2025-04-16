36e87b72891c570b6e292aa8899c7744db21166e
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-16 20:53:36 +0530

    adding and removing user from browse page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,24 @@
+import { signOut } from 'firebase/auth'
 import React from 'react'
-import { useState } from "react";
+import { useState } from 'react'
+import { auth } from '../utils/firebase'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false)
+  const navigate = useNavigate()
+  const handleSignOut = () => {
+    // Logic to sign out the user
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      navigate('/')
+    }).catch((error) => {
+      // An error happened.
+      navigate('/error')
+      console.error('Sign out error:', error)
+    });
+    console.log('User signed out')
+  }
 
   return (
     <header className="absolute top-0 left-0 w-full px-8 py-4 bg-gradient-to-b from-black z-30 flex items-center justify-between">
@@ -27,7 +43,7 @@ const Header = () => {
 
           <svg
             className={`w-4 h-4 text-white transition-transform ${
-              showMenu ? "rotate-180" : ""
+              showMenu ? 'rotate-180' : ''
             }`}
             fill="none"
             stroke="currentColor"
@@ -45,15 +61,24 @@ const Header = () => {
         {/* Dropdown Menu */}
         {showMenu && (
           <ul className="absolute top-12 right-0 w-40 bg-black bg-opacity-90 text-white shadow-lg rounded-md py-2 z-50">
-            <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Profile</li>
-            <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Settings</li>
+            <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">
+              Profile
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">
+              Settings
+            </li>
             <li className="border-t border-gray-600 my-1"></li>
-            <li className="px-4 py-2 text-red-400 hover:bg-red-600 cursor-pointer hover:text-black">Sign Out</li>
+            <button
+              className="px-4 w-40 py-2 text-red-400 hover:bg-red-600 cursor-pointer hover:text-black"
+              onClick={handleSignOut}
+            >
+              Sign Out
+            </button>
           </ul>
         )}
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
