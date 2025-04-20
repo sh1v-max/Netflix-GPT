005582817a8946f84ad17f9b5fb7a55fb92d3a2e
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-20 13:56:04 +0530

    setting up redux store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,26 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constant'
-import { useDispatch } from 'react-redux'
-import {addNowPlayingMovies} from '../Utils/movieSlice'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 
 const Browse = () => {
-  const dispatch = useDispatch()
-  const getNowPlaying = async () => {
-    const data = await fetch(
-      'https://api.themoviedb.org/3/movie/now_playing?page=1',
-      API_OPTIONS
-    )
-    const json = await data.json()
-    console.log(json.results)
-    // adding data into the store
-    dispatch(addNowPlayingMovies(json.results))
-  }
 
-  useEffect(() => {
-    getNowPlaying()
-  }, [])
-
+  useNowPlayingMovies()
+  
   return (
     <>
       <Header />
---
+++ src/hooks/useNowPlayingMovies.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from "react-redux"
+import { API_OPTIONS } from "../utils/constant"
+import { addNowPlayingMovies } from "../Utils/moviesSlice"
+import { useEffect } from "react"
+
+const useNowPlayingMovies = () => {
+  // fetching data from TMDB and update store
+  const dispatch = useDispatch()
+  const getNowPlaying = async () => {
+    const data = await fetch(
+      'https://api.themoviedb.org/3/movie/now_playing?page=1',
+      API_OPTIONS
+    )
+    const json = await data.json()
+    console.log(json.results)
+    // adding data into the store
+    dispatch(addNowPlayingMovies(json.results))
+  }
+
+  useEffect(() => {
+    getNowPlaying()
+  }, [])
+}
+
+export default useNowPlayingMovies 
