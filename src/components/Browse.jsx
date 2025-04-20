ea4b5cce59f5debedc574abb2fba0ab84a64180f
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-20 15:02:57 +0530

    main and secondary container

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Header from './Header'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
+import MainContainer from './mainContainer'
+import SecondaryContainer from './secondaryContainer'
 
 const Browse = () => {
 
   useNowPlayingMovies()
   
   return (
-    <>
+    <div>
       <Header />
-      <div>Browse</div>
-    </>
+      <MainContainer />
+      <SecondaryContainer />
+    </div>
   )
 }
 
