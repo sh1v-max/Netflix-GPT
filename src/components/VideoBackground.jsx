5c54398b3f49e288f022985ceaaebc89e20dd39d
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-21 16:51:23 +0530

    custom hook for movie trailers

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,34 +1,10 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constant'
-import { useDispatch, useSelector } from 'react-redux'
-import { addTrailerVideo } from '../Utils/moviesSlice'
+import React from 'react'
+import {useSelector } from 'react-redux'
+import useMovieTrailer from '../hooks/useMovieTrailer'
 
 export const VideoBackground = ({ movieId }) => {
-  // but we won't be using states, we'll rather use redux store
-  // const [trailerKey, setTrailerKey] = useState(null)
   const trailerVideo = useSelector(store => store.movies?.trailerVideo)
-  const dispatch = useDispatch()
-  //  fetch trailer video
-
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTIONS
-    )
-    const json = await data.json()
-    console.log(json)
-    // Filter for trailer type
-    const filterData = json.results.filter((video) => video.type === 'Trailer')
-    // if no trailer found
-    const trailer = filterData.length ? filterData[0] : json.results[0]
-    dispatch(addTrailerVideo(trailer))
-    console.log(trailer)
-  }
-
-  useEffect(() => {
-    getMovieVideos()
-  }, [])
-
+  useMovieTrailer(movieId)
   return (
     <div>
       <iframe
---
+++ src/hooks/useMovieTrailer.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react"
+import { addTrailerVideo } from "../Utils/moviesSlice"
+import { API_OPTIONS } from "../utils/constant"
+import { useDispatch } from "react-redux"
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch()
+
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+      API_OPTIONS
+    )
+    const json = await data.json()
+    console.log(json)
+    // Filter for trailer type
+    const filterData = json.results.filter((video) => video.type === 'Trailer')
+    // if no trailer found
+    const trailer = filterData.length ? filterData[0] : json.results[0]
+    dispatch(addTrailerVideo(trailer))
+    console.log(trailer)
+  }
+
+  useEffect(() => {
+    getMovieVideos()
+  }, [])
+}
+
+export default useMovieTrailer
