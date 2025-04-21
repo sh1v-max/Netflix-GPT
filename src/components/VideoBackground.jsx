9ef883e1a59b2c7b8ae2b15f436d60a5364a8d22
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-21 16:25:09 +0530

    added trailer in redux store

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { API_OPTIONS } from '../utils/constant'
 
 export const VideoBackground = ({ movieId }) => {
-  console.log(movieId)
+  const [trailerKey, setTrailerKey] = useState(null)
   //  fetch trailer video
 
   const getMovieVideos = async () => {
@@ -16,6 +16,9 @@ export const VideoBackground = ({ movieId }) => {
     const filterData = json.results.filter((video) => video.type === 'Trailer')
     // if no trailer found
     const trailer = filterData.length ? filterData[0] : json.results[0]
+    if (trailer) {
+      setTrailerKey(trailer.key)
+    }
     console.log(trailer)
   }
 
@@ -23,7 +26,18 @@ export const VideoBackground = ({ movieId }) => {
     getMovieVideos()
   }, [])
 
-  return <div>VideoBackground</div>
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${trailerKey}?si=PNj9WG1WzL7MyUGN`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+      ></iframe>
+    </div>
+  )
 }
 
 export default VideoBackground
--- src/utils/moviesSlice.jsx
+++ src/utils/moviesSlice.jsx
@@ -4,23 +4,18 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    trailerVideo: null,
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
-    // addPopularMovies: (state, action) => {
-    //   state.popularMovies = action.payload;
-    // },
-    // addTopRatedMovies: (state, action) => {
-    //   state.topRatedMovies = action.payload;
-    // },
-    // addUpcomingMovies: (state, action) => {
-    //   state.upcomingMovies = action.payload;
-    // }
+    addTrailerVideo : (state, action) => {
+      state.trailerVideo = action.payload
+    }
   },
 })
 
-export const { addNowPlayingMovies } = moviesSlice.actions
+export const { addNowPlayingMovies, addTrailerVideo } = moviesSlice.actions
 
 export default moviesSlice.reducer;
