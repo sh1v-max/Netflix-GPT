a77c1c02e740abbcb9aa9a191f14a9a17465f9e0
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-21 22:39:55 +0530

    styling secondary container

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -9,6 +9,10 @@ const SecondaryContainer = () => {
   return (
     <div>
       <MovieList title = {"Now Playing"} movies = {movies.nowPlayingMovies}/>
+      <MovieList title = {"Trending"} movies = {movies.nowPlayingMovies}/>
+      <MovieList title = {"Popular"} movies = {movies.nowPlayingMovies}/>
+      <MovieList title = {"Upcoming Movies"} movies = {movies.nowPlayingMovies}/>
+      <MovieList title = {"Horror"} movies = {movies.nowPlayingMovies}/>
     </div>
   )
 }
