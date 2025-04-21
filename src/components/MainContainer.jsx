95cdfb7190d827ec5379782e9fe218919c00cadc
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-21 13:02:36 +0530

    getting background video data

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,19 +4,21 @@ import VideoTitle from './VideoTitle'
 import VideoBackground from './VideoBackground'
 
 const MainContainer = () => {
-  const movies = useSelector(store => store.movies?.nowPlayingMovies)
-  
-  if (!movies) return
-  
-  const mainMovie = movies[0]
-  console.log(mainMovie)
+  const movies = useSelector(store => store.movies?.nowPlayingMovies);
 
-  const {original_title, overview} = mainMovie
+  if (!movies) return;
+  
+  const randomIndex = Math.floor(Math.random() * movies.length);
+  const mainMovie = movies[randomIndex];
+  
+  console.log(mainMovie);
+
+  const {title, overview, id} = mainMovie
   
   return (
     <div>
-      <VideoTitle title = {original_title} overview = {overview}/>
-      <VideoBackground/>
+      <VideoTitle title = {title} overview = {overview}/>
+      <VideoBackground movieId={id}/>
     </div>
   )
 }
--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
-export const VideoBackground = () => {
+export const VideoBackground = ({movieId}) => {
+  console.log(movieId)
+  //  fetch trailer video
+
+  const getMovieVideos = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/1197306/videos?language=en-US', options)
+
+  }
+  
   return (
     <div>VideoBackground</div>
   )
