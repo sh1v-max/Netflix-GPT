5cbf75bc8c939964e803624d53cda2b97d4b4969
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-23 22:12:39 +0530

    search for gpt

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const GptSearchBar = () => {
   return (
-    <div>GptSearchBar</div>
+    <div>
+      <form className=" p-2 m-30 border-black" action="">
+        <input
+          type="text"
+          className=" p-4 m-4 border-black"
+          placeholder="What do you like to watch today..."
+        />
+        <button className='p-2 m-2 border-black'>Search</button>
+      </form>
+    </div>
   )
 }
 
