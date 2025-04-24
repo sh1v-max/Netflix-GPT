b8550f0826a740353afa0b9cd198f44ffe0cac81
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-24 17:31:35 +0530

    fixing login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -106,14 +106,14 @@ const Login = () => {
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword)
   }
-
+  
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="relative min-h-screen">
       {/* Header fixed at top */}
       <Header />
 
       {/* Hero section with full height */}
-      <div className="relative h-screen w-full flex items-center justify-center">
+      <div className="relative h-screen w-full">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="Background"
