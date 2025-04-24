68ff15cfdc967ea3bfad27c54ed010984468f197
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-24 18:24:40 +0530

    fixing login page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { IMG_CDN_URL } from '../utils/constant'
 
-const MovieCard = ({ posterPath }) => {
-  // console.log(posterPath)
+const MovieCard = ({posterPath}) => {
+  console.log(posterPath)
+
   return (
     <div className="w-48 pr-4 relative group">
       <div className="rounded-lg overflow-hidden shadow-md transform transition duration-300 group-hover:scale-95 group-hover:z-20 relative">
@@ -11,25 +12,32 @@ const MovieCard = ({ posterPath }) => {
           alt="Movie Card"
           className="w-full h-auto object-cover"
         />
-
-        {/* Play Button */}
-        <div className=''>
-          <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            <button className="bg-white text-black rounded-full p-2 shadow-lg hover:scale-110  cursor-pointer transition-transform duration-200">
+  
+        {/* Hover Icons Container */}
+        <div>
+          {/* Play Button - Bottom Left */}
+          <div className="absolute bottom-2 left-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <button className="bg-white text-black rounded-full p-2 shadow-lg hover:scale-110 cursor-pointer transition-transform duration-200">
               ▶️
             </button>
           </div>
-
-          {/* Three Dots */}
+  
+          {/* Three Dots - Top Right */}
           <div className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            <button className="text-white bg-black bg-opacity-60 rounded-full p-1 px-2 hover:bg-opacity-80  cursor-pointer">
+            <button className="text-white bg-black bg-opacity-60 rounded-full p-1 px-2 hover:bg-opacity-80 cursor-pointer">
               ⋮
             </button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
+  
+  // return (
+  //   <div className = "w-48 pr-4">
+  //     <img src={IMG_CDN_URL + posterPath} alt="Movie Card" />
+  //   </div>
+  // )
 }
 
 export default MovieCard
