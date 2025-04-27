34cfbe2716b2d997cc592034d99b853c7bbbde65
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-27 17:45:28 +0530

    memoisation

--- src/hooks/useMovieTrailer.jsx
+++ src/hooks/useMovieTrailer.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react"
 import { addTrailerVideo } from "../Utils/moviesSlice"
 import { API_OPTIONS } from "../utils/constant"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch()
+
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo)
 
   const getMovieVideos = async () => {
     const data = await fetch(
@@ -18,7 +20,7 @@ const useMovieTrailer = (movieId) => {
   }
 
   useEffect(() => {
-    getMovieVideos()
+    !trailerVideo && getMovieVideos()
   }, [])
 }
 
