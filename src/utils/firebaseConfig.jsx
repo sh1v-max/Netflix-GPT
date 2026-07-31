fc6b35155e04aab73302a9392fa89028bb5ca449
sh1v-max <singhshiv0427@gmail.com>
2026-07-31 17:40:19 +0530

    updated firebase version

--- src/utils/firebaseConfig.jsx
+++ src/utils/firebaseConfig.jsx
@@ -18,7 +18,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
-export const auth = getAuth()
+export const auth = getAuth(app)
